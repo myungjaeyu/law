016c15c73b14a22a93041594abec78a8e9e02eee
myungjaeyu <rootcode10@gmail.com>
2021-08-04 16:28:13 +0900

    feat: add config/styles.

---
+++ config/styles.ts
@@ -0,0 +1,7 @@
+export const media = {
+    common: '@media only screen and (max-width: 768px)',
+    phone_s: '@media only screen and (min-width: 330px)',
+    phone: '@media only screen and (min-width: 576px)',
+    tablet: '@media only screen and (min-width: 768px)',
+    desktop: '@media only screen and (min-width: 961px)'
+}
